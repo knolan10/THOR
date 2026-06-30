8e80941faa4dffa48a6015943965786e8b9edbf2
Kira <ksn700@gmail.com>
2026-06-30 08:52:51 -0700

    initial setup

---
+++ src/thor/alert_stream/catalogs_catalog.py
@@ -0,0 +1,34 @@
+catalogs = {
+    "COSMOS2025_cut.fits": {
+        "link": 'https://cosmos2025.iap.fr/',
+        "ra_range": [149.6641, 150.5770],
+        "dec_range": [1.7294, 2.6873],
+        "z_max": 14.9463,
+        "z_median": 1.1406,
+        "number_objects": 594684,
+    },
+    "CLAUDS_SourceExtractor_COSMOS_cut.fits": {
+        "link": 'https://www.clauds.net/available-data',
+        "ra_range": [149.2942, 150.7861],
+        "dec_range": [1.5976, 2.8102],
+        "z_max": 6.0000,
+        "z_median": 1.6264,
+        "number_objects": 899115,
+    },
+    "CLAUDS_SourceExtractor_XMMLSS_cut.fits": {
+        "link": 'https://www.clauds.net/available-data',
+        "ra_range": [33.8510, 37.1751],
+        "dec_range": [-5.6290, -3.9996],
+        "z_max": 6.0000,
+        "z_median": 1.4889,
+        "number_objects": 1807274,
+    },
+    "CLAUDS_SourceExtractor_DEEP23_cut.fits": {
+        "link": 'https://www.clauds.net/available-data',
+        "ra_range": [350.2710, 354.1673],
+        "dec_range": [-1.5791, 1.2104],
+        "z_max": 6.0000,
+        "z_median": 1.4165,
+        "number_objects": 2637763,
+    }
+}
